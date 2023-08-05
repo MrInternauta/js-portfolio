d9b388bfc1ebf2868177962f26d7a8c35b29e268
Felipe Ramirez <framirez@bwl.com.mx>
2023-08-04 21:40:06 -0500

    initial

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import '@styles/main.css';
 import '@styles/vars.styl';
 
 (async function App() {
+
+
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
 })();
